aec96faf7c24552805c658432e11eae9cc73d8e0
Karl Entwistle <karl@entwistle.com>
2011-04-16 17:13:39 +0100

    sample doesnt work in 1.8.7

--- lib/cherrypicker/functions.rb
+++ lib/cherrypicker/functions.rb
@@ -49,5 +49,10 @@ def random_agent
       "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-us) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
       "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.151 Safari/534.16",
       "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)",
-      "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.14) Gecko/20110218 Firefox/3.6.14"].sample
+      "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.14) Gecko/20110218 Firefox/3.6.14"]
+    @useragent[rand(14)]
 end
+
+
+
+p random_agent
