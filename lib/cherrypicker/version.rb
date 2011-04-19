ab7d8169125c5fb6ae57ae9b7dc57ce92e80aab8
Karl Entwistle <karl@entwistle.com>
2011-04-19 14:03:49 +0100

    rapidshare now sends response as a 302 redirect must be parsed to find hostname

--- lib/cherrypicker/version.rb
+++ lib/cherrypicker/version.rb
@@ -1,3 +1,3 @@
 module Cherrypicker
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 end
--- test/unit/rapidshare_test.rb
+++ test/unit/rapidshare_test.rb
@@ -5,7 +5,7 @@ class RapidshareTest < Test::Unit::TestCase
   context "Information request" do
          
       should "return response" do
-        response = Rapidshare.new("http://rapidshare.com/files/453165880/test_file.txt", "6160038", "PXVA62ZY")
+        response = Rapidshare.new("http://rapidshare.com/files/453165880/test_file.txt", "username", "password")
         assert response.fileid =~ /\d*/
         assert response.filename.length > 0
         assert response.hostname.length > 0
