885200858695a702dff7300c41cbe656b07cd863
Karl Entwistle <karl@entwistle.com>
2011-05-04 21:49:16 +0100

    random browser agent is active again, might improve download speeds

--- lib/cherrypicker/helpers.rb
+++ lib/cherrypicker/helpers.rb
@@ -9,7 +9,6 @@ require 'net/http'
 require 'net/https'
 require 'progressbar'
 require 'open-uri'
-require 'progress'
 
 module Cherrypicker
   def self.remote_query(url)  
@@ -76,7 +75,7 @@ module Cherrypicker
         end
       end
     end
-    
+        
     @finished = true
     puts
     puts "download completed"
@@ -91,7 +90,18 @@ module Cherrypicker
   end
 
   def self.random_agent
-    @useragent = ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_7) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.50 Safari/534.24"]
-    return @useragent[0]
+    useragent = [ "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.1 (KHTML, like Gecko) Chrome/4.0.219.6 Safari/532.1",
+    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)",
+    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.5.30729; .NET CLR 3.0.30729)",
+    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Win64; x64; Trident/4.0)",
+    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SV1; .NET CLR 2.0.50727; InfoPath.2)Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)",
+    "Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_7) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.50 Safari/534.24"]
+    return useragent[rand(useragent.length)]
   end
 end
--- lib/cherrypicker/version.rb
+++ lib/cherrypicker/version.rb
@@ -1,3 +1,3 @@
 module Cherrypicker
-  VERSION = "0.3.13"
+  VERSION = "0.3.14"
 end
